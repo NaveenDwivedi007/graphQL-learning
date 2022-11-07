8257c2bf37f7aaed4b0d859b5e4515dfd88c4328
Naveen Dwivedi <naveen.kumar2@pristyncare.com>
2022-11-07 09:00:06 +0530

    basic graph ql server created

---
+++ demo.js
@@ -0,0 +1,48 @@
+const gql = require('graphql-tag');
+const {ApolloServer} = require('apollo-server');
+
+/**
+ * "!" mark in type defination is to mark them 
+ * as it should alway be present 
+ */
+
+/**
+ * 
+ * schema always need a query to access that schema
+ * 
+ */
+const typeDefs = gql`
+  type User {
+    email: String! 
+    avatar:String
+    friends:[User]!
+  }
+
+  type Query {
+    me:User!
+  }
+`
+// resolver have a same value type as type defs
+const resolvers = {
+  Query:{
+    me(){
+      return {
+        email:'naveen@naveen.com',
+        avatar:'http://yoda.pnd',
+        friends:[]
+      }
+    }
+  }
+}
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+server.listen(4000)
+.then(()=>{
+  console.log('server running');
+})
+.catch((err)=>{
+  console.error(err);
+})
